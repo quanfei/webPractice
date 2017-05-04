b60fc0aceb1370802269a837b89ddd0f362d3d56
quanfei <apple@appletekiMacBook-Air.local>
2017-05-03 20:08:21 -0400

    everything except map

---
+++ django/companysite/intro_site/tests.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+
+# Create your tests here.
+def index(request):
+        return render(
+        request,
+        'index.html',
+    )
--- django/companysite/intro_site/urls.py
+++ django/companysite/intro_site/urls.py
@@ -1,9 +1,6 @@
 from django.conf.urls import url
-
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^about',views.about,name='about'),
-    url(r'^products',views.products,name='products')
 ]
