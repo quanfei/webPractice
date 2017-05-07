84e89375afb3af7259dbc3e77e9f4cf039f2d685
System Administrator <root@tests-MacBook-Pro-2.local>
2017-05-06 21:34:53 -0400

    update pic

--- django/companysite/companysite/settings.py
+++ django/companysite/companysite/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-   ]
+]
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
 
--- django/companysite/intro_site/views.py
+++ django/companysite/intro_site/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+from .forms import ContactForm
 
 # Create your views here.
 def index(request):
-        return render(
+    return render(
+        request,
+        'index.html',
+    )
+
+def contactinfo(request):
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        print("ok")
+        if form.is_valid():
+            print("ok")
+            name= form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            subject = form.cleaned_data['subject']
+            comment = form.cleaned_data['commet']
+
+            recipients = ['edisonxmt@gmail.com']
+            
+
+            send_mail(subject, comment, email, recipients)
+    return render(
         request,
         'index.html',
     )
